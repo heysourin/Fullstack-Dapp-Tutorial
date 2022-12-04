14df9955188b527034efd0719ca609b2aa1262bb
heysourin <sourin07@gmail.com>
2022-12-04 16:17:41 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+//'Greeter.sol' in the above line is a folder
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+function App() {
+  const [greeting, setGreetingValue] = useState("");
+
+  // We need a fucntion to fetch data from blockchain to frontend
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+
+      try {
+        const data = await contract.greet();
+        setGreetingValue(data);
+        console.log(`Greet data: ${data}`);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
+  async function setGreeting(value) {
+    if (!value) return;
+    if (!typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(value);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await setGreeting(event.target.greetingInput.value);
+    setGreetingValue(event.target.greetingInput.value);
+    event.target.greetingInput.value = "";
+  }
+  return (
+    <div className="w-full max-w-lg container">
+      <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4">
+        <div className="text-gray-600 font-bold text-lg mb-2">
+          React Ethereum Dapp
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Fetch Greeting Message From Smart Contract
+          </div>
+          <div className="flex ">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={fetchGreeting}
+            >
+              Fetch Greeting
+            </button>
+          </div>
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Set Greeting Message On Smart Contract
+          </div>
+          <form
+            className="flex items-center justify-between"
+            onSubmit={(event) => handleSubmit(event)}
+          >
+            <input
+              className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="greetingInput"
+            />
+            <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Set Greeting
+            </button>
+          </form>
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400 bg-gray-100">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Greeting Message
+          </div>
+          <p>{greeting}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
